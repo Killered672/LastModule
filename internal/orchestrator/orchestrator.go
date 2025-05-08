6280cd0ccf36ba2455c03f99e9d9ce001eae611b
Killered672 <maslenok1234567890@gmail.com>
2025-05-08 13:32:00 +0300

    fix

--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -484,9 +484,9 @@ func (o *Orchestrator) RunServer() error {
 	}()
 
 	protected := http.NewServeMux()
-	protected.HandleFunc("/calculate", o.calculateHandler)
-	protected.HandleFunc("/expressions", o.expressionsHandler)
-	protected.HandleFunc("/expressions/", o.expressionIDHandler)
+	protected.HandleFunc("/api/v1/calculate", o.calculateHandler)
+	protected.HandleFunc("/api/v1/expressions", o.expressionsHandler)
+	protected.HandleFunc("/api/v1/expressions/", o.expressionIDHandler)
 	protected.HandleFunc("/internal/task", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			o.getTaskHandler(w, r)
@@ -498,7 +498,7 @@ func (o *Orchestrator) RunServer() error {
 	})
 
 	mux := http.NewServeMux()
-	mux.Handle("/api/v1/", o.authMiddleware(protected))
+	mux.Handle("/", o.authMiddleware(protected))
 	mux.HandleFunc("/api/v1/login", o.loginHandler)
 	mux.HandleFunc("/api/v1/register", o.registerHandler)
 
