f474330086a50da70b8ba3d96b6968d95d6f17bf
Killered672 <maslenok1234567890@gmail.com>
2025-05-10 11:40:15 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
     }
   },
   server: {
-    host: true,
+    port: 5174,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
